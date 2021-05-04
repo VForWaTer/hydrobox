6c0eaee6470e37b7032de6f071b1788d8b86cbc1
Mirko Mälicke <mirko@hydrocode.de>
2021-05-04 13:18:02 +0200

    variogram added

--- hydrobox/geostat/__init__.py
+++ hydrobox/geostat/__init__.py
@@ -1,9 +1,20 @@
 """
-Geostatistics Package
-=====================
+Geostatistical Toolbox
+----------------------
 
-The geostatistics package of hydrobox makes use of two other
-great packages: GSTools and SciKit-GStat.
+This toolbox collects all functions needed to run different geostatistical
+tasks. Variogram analysis can be done with :func:`variogram <hydrobox.geostat.variogram>`,
+which wraps the scikit-gstat Variogram class.
 
+.. minigallery:: hydrobox.geostat.variogram
+    :add-heading: Geostatistics examples
+
+Content of :py:mod:`hydrobox.geostat`:
+
+.. autosummary:: 
+    :toctree: gen_modules/
+    :template: module.rst
+
+    variogram
 """
 from .variogram import variogram
--- hydrobox/geostat/typing.py
+++ hydrobox/geostat/typing.py
@@ -1,4 +1,5 @@
 from typing import Union, Callable, Literal
+import numpy as np
 
 # some typings
 Estimator = Union[
