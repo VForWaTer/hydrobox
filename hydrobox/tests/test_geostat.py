5914459b822c23a4f458611edcb688efdb3bceae
Mirko Mälicke <mirko@hydrocode.de>
2021-05-04 15:33:18 +0200

    gridsearch test

--- hydrobox/tests/test_geostat.py
+++ hydrobox/tests/test_geostat.py
@@ -2,6 +2,7 @@ from hydrobox import data
 import hydrobox
 import plotly.graph_objects as go
 import skgstat as skg
+from sklearn.model_selection import GridSearchCV
 
 
 def test_variogram():
@@ -18,3 +19,41 @@ def test_variogram():
         )
 
         assert isinstance(vario, type_)
+
+
+def test_gridsearch():
+    """Test Gridsearch with explicit param_grid"""
+    # create the grid
+    param_grid={'model': ('spherical', 'gaussian', 'stable')}
+
+    # get data
+    df = data.pancake()
+    
+    gs = hydrobox.geostat.gridsearch(
+        param_grid=param_grid,
+        coordinates=df[['x', 'y']].values,
+        values=df.z.values,
+        n_lags=25,
+        return_type='object'
+    )
+
+    assert isinstance(gs, GridSearchCV)
+
+def test_gridsearch_variogram():
+    """Test Gridsearch with variogram passed"""
+    df = data.pancake()
+    # create the grid
+    param_grid={'fit_method': ('trf', 'lm', 'ml')}
+    vario = hydrobox.geostat.variogram(
+            df[['x', 'y']].values,
+            df.z.values,
+            n_lags=15,
+            return_type='object'
+        )
+    gs = hydrobox.geostat.gridsearch(
+        param_grid,
+        variogram=vario,
+        return_type='best_param'
+    )
+
+    assert isinstance(gs, dict)
