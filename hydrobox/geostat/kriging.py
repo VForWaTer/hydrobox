3705dd8c206aa4df424691379cb8df59d1e0c6a5
Mirko Mälicke <mirko@hydrocode.de>
2021-05-05 12:23:29 +0200

    sync kriging file

---
+++ hydrobox/geostat/kriging.py
@@ -0,0 +1,12 @@
+from typing import List, Union
+import numpy as np
+import skgstat as skg
+import plotly.graph_objects as go
+
+def kriging(
+    variogram: skg.Variogram = None,
+    **kwargs,
+) -> Union[List[np.ndarray, np.ndarray], go.Figure]:
+    """
+    """
+    pass 
