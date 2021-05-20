2e3fcc7e92941cf57a7a4da8fcc57d8fb1a0fc11
Mirko Mälicke <mirko@hydrocode.de>
2021-05-20 09:39:37 +0200

    add github link

--- doc/conf.py
+++ doc/conf.py
@@ -56,6 +56,10 @@ exclude_patterns = []
 #
 html_theme = 'pydata_sphinx_theme'
 
+html_theme_options = {
+    'github_url': 'https://github.com/vforwater/hydrobox',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
